62fcca531e37bb4c84d0e331927a74cde9d8fa10
Connor van Spronssen <info@dobefu.com>
2024-11-19 07:19:56 +0100

    Add a test for the MinInt function

--- math/min-int_test.go
+++ math/min-int_test.go
@@ -4,20 +4,20 @@ import (
 	"testing"
 )
 
-func TestMaxInt(t *testing.T) {
+func TestMinInt(t *testing.T) {
 	assertions := []struct {
 		arg1   int
 		arg2   int
 		output int
 	}{
-		{1, 2, 2},
-		{2, 1, 2},
+		{1, 2, 1},
+		{2, 1, 1},
 		{1, 1, 1},
-		{-2, 1, 1},
+		{-2, 1, -2},
 	}
 
 	for _, assertion := range assertions {
-		result := MaxInt(assertion.arg1, assertion.arg2)
+		result := MinInt(assertion.arg1, assertion.arg2)
 
 		if result != assertion.output {
 			t.Fatalf(
